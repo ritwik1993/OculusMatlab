6e9ee5ccd725faf82c4b4f03ea32d24d605dde44
Ritwik Ummalaneni <ritwik1993@gmail.com>
2015-10-29 17:44:39 -0500

    Added author info

--- src/include/Common.h
+++ src/include/Common.h
@@ -3,6 +3,8 @@
  Authors     :   Bradley Austin Davis <bdavis@saintandreas.org>
  Copyright   :   Copyright Brad Davis. All Rights reserved.
 
+ Modified by :   Ritwik Ummalaneni <ritwik1993@gmail.com> 
+ Copyright   :   Copyright Ritwik Ummalaneni. All Rights reserved.
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
